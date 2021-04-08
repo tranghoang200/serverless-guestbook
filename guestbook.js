e0b0bd70c8501abaded6b7b2e300a44cd14a42d0
Trang Hoang <49129207+tranghoang200@users.noreply.github.com>
2021-04-08 22:20:49 +0700

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://e9dc511c.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://e9dc511c.us-south.apigw.appdomain.cloud/guestbook/entries';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
